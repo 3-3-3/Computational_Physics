1add74817eb9a4fb28ae961260b48ede3c3b5b79
Liam Keeley <liamkeeley@Liams-MacBook-Pro.local>
2021-09-04 17:38:43 -0600

    Something wrong with my least squares solutioin

---
+++ Quick_Tour_Quiz/q1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ppm_mauna = np.load("Mauna_CO2.npy")
+s = ppm_mauna.size
+
+#Let t=0 be January 1st. Then, the last date that data was collected is s*14, where s is the number
+#Of measurements taken and 14 is the days between measurements.
+t_array = np.linspace(0,s*14,s)
+
+#Use a least-squares line to fit the data
+#Let b = [b_0, b1], where b_0 is the y-intercept and b1 is the slope
+#Let X = [[1, t_array[0]]...[1, t_array[len(t_array)]]]
+#Let y = ppm_mauna. Then, the least squares line is the best fit to Xb = y
+
+X = np.array([[1, t_array[i]] for i in range(len(t_array))])
+
+#solve for b.
+
+XXT = np.dot(np.transpose(X),X)
+XTy = np.dot(np.transpose(X), ppm_mauna)
+
+#b is the the inverse of XXT times XTy
+iXXT = (1/np.linalg.det(XXT))*np.array([[XXT[1][1], XXT[0][1]], [XXT[1][0], XXT[0][0]]])
+b = np.dot(iXXT, XTy)
+print(b)
+
+#Create the best fit
+y = np.array([b[1]*t_array[i] for i in range(len(t_array))])
+
+plt.scatter(t_array, ppm_mauna)
+plt.plot(t_array, y)
+plt.show()
