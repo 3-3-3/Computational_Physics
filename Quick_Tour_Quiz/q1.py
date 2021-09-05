b14f00728c5b43c5288d1a5b7976ffc0d6024c7d
Liam Keeley <liamkeeley@Liams-MacBook-Pro.local>
2021-09-04 19:46:29 -0600

    Tried to add a cyclical sin term to best fit. Did not really work.

--- Quick_Tour_Quiz/q1.py
+++ Quick_Tour_Quiz/q1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.constants as sc
 
 ppm_mauna = np.load("Mauna_CO2.npy")
 s = ppm_mauna.size
@@ -27,6 +28,27 @@ print(b)
 #Create the best fit
 y = np.array([b[0] + b[1]*t_array[i] for i in range(len(t_array))])
 
+#Can we get a better fit? There is clearly a cyclical nature to the readings in addition to the upward trend.
+#Fit the equation y=b_0f_0(x) + b_1f_1(x) + b_2f_2(x)
+#Such that f_0(x) = 1, f_2(x) = 2, and f_3(x) = sin(ax)
+#With a = 2pi/365
+
+a = (2*sc.pi)/185
+
+X_2 = np.array([[1, i, np.sin(a*i), np.cos(a*i)] for i in t_array])
+X_2X_2T = np.dot(np.transpose(X_2), X_2)
+X_2Ty = np.dot(np.transpose(X_2), y)
+
+b_2 = np.linalg.solve(X_2X_2T, X_2Ty)
+
+y_2 = np.array([b_2[0] + b_2[1]*i + b_2[2]*i for i in t_array])
+print('b_2')
+print(b_2)
+
+plt.title('CO2 Levels In Mauna Loa')
+plt.ylabel('CO2 Levels (Parts Per Million)')
+plt.xlabel('Days Since 1981')
 plt.scatter(t_array, ppm_mauna)
 plt.plot(t_array, y)
+plt.plot(t_array, y_2)
 plt.show()
