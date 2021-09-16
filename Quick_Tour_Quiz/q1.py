681f39b40452a9895ce95fff2d3d8c5d17f41af9
Liam Keeley <liamkeeley@Liams-MacBook-Pro.local>
2021-09-16 12:53:33 -0600

    Fixed little problem

--- Quick_Tour_Quiz/q1.py
+++ Quick_Tour_Quiz/q1.py
@@ -15,17 +15,16 @@ t_array = np.linspace(0,s*14,s)
 #Let b = [b_0, b1], where b_0 is the y-intercept and b1 is the slope
 #Let X = [[1, t_array[0]]...[1, t_array[len(t_array)]]]
 #Let y = ppm_mauna. Then, the least squares line is the best fit to Xb = y
+a = 2*sc.pi/365
+f = [lambda f_0 : 1, lambda f_1 : f_1, lambda f_3 : np.sin(a*f_1)]
 
-def ls_a_x_sinx(x, y, a=1):
-    #Can we get a better fit? There is clearly a cyclical nature to the readings in addition to the upward trend.
-    #So consider the function space spanned by {1,x,sin(ax)} instead of {1,x}
-    #Fit the equation y=b_0f_0(x) + b_1f_1(x) + b_2f_2(x)
-    #Such that f_0(x) = 1, f_2(x) = 2, and f_3(x) = sin(ax)
-    #With a = 2pi/365
-    #x and y are numpy arrays.
-    #Define the matrix A where each row is [1, x_n, sin(a*x_n)] for all x_n in x
-    #a is the period of the sin function which is to be fitted
-    A = np.array([[1, x_n, np.sin(a*x_n)] for x_n in x])
+
+def ls(x, y, f_space):
+    #x: x data to fit
+    #y: y data to fit
+    #f_space: function space. List of python functions representing
+    #mathematical functions
+    A = np.array([[i(x) for i in f_space] for x_n in x])
     AT = np.transpose(A)
     #Let B=[b_0, b_1, b_2]. Then, return the solution to [AT.A].B = [AT].y
     print(f'A transpose dot A: {np.dot(AT, A)}')
@@ -35,35 +34,28 @@ def ls_a_x_sinx(x, y, a=1):
 
 
 
-#dummy data
-x = np.array([sc.pi/2*i for i in range(10)])
-y = np.array([5-2*i+4*np.sin(1.2*i)+0.1*random.randrange(1,10) for i in x])
+if __name__ == '__main__':
+    #dummy data
+    x = np.array([sc.pi/2*i for i in range(10)])
+    y = np.array([5-2*i+4*np.sin(1.2*i)+0.1*random.randrange(1,10) for i in x])
 
 
-#x_b = np.linspace(0,4*sc.pi,100)
-a = 2*sc.pi/365
-b = ls_a_x_sinx(t_array,ppm_mauna,a)
-print(f'b (solution to AT.A.[b] = AT.y): {b}')
-y_b = np.array([b[0] + b[1]*i + b[2]*np.sin(a*i) for i in t_array])
+    #x_b = np.linspace(0,4*sc.pi,100)
+    b = ls_a_x_sinx(t_array,ppm_mauna,a)
+    print(f'b (solution to AT.A.[b] = AT.y): {b}')
+    y_b = np.array([b[0] + b[1]*i + b[2]*np.sin(a*i) for i in t_array])
 
-def rising_sine_function(x, b_0, b_1, b_2, a, p):
-    y = b_0 + b_1*x + b_2*np.sin((2*sc.pi*a)*x + p)
-    return y
 
-opt, pcov = curve_fit(rising_sine_function, t_array, ppm_mauna)
 
-print(f'opt: {opt}')
+    plt.title(f'$CO_2$ Levels In Mauna Loa (Average increase of {np.round(b[1]*365, decimals=2)} ppm per year)')
+    plt.ylabel('$CO_2$ Levels (Parts Per Million)')
+    plt.xlabel('Days Since 1981')
+    #plt.scatter(x,y)
+    text_kwargs = dict(ha='center', va='center', fontsize=28, color='C1')
+    plt.plot(t_array, y_b)
+    plt.scatter(t_array, ppm_mauna, c=['orange'], marker='.')
+    #note that b[1] is the average increase per day. Multiply by 365 to obtain average increase per day.
 
-plt.title(f'$CO_2$ Levels In Mauna Loa (Average increase of {np.round(b[1]*365, decimals=2)} ppm per year)')
-plt.ylabel('$CO_2$ Levels (Parts Per Million)')
-plt.xlabel('Days Since 1981')
-#plt.scatter(x,y)
-text_kwargs = dict(ha='center', va='center', fontsize=28, color='C1')
-plt.plot(t_array, y_b)
-plt.plot(t_array, rising_sine_function(t_array, *opt))
-plt.scatter(t_array, ppm_mauna, c=['orange'], marker='.')
-#note that b[1] is the average increase per day. Multiply by 365 to obtain average increase per day.
-
-#plt.plot(t_array, y_2)
-plt.savefig('Mauna_CO2.png')
-plt.show()
+    #plt.plot(t_array, y_2)
+    plt.savefig('Mauna_CO2.png')
+    plt.show()
