bb782e5000b9372243e8c91eeb3c66483fdeebd6
Liam Keeley <liamkeeley@Liams-MacBook-Pro.local>
2021-10-01 11:58:00 -0600

    finished excercise 2

--- File_IO/e1.py
+++ File_IO/e1.py
@@ -8,4 +8,4 @@ s_60hz = lambda x : np.sin((60*2*np.pi)*x)
 s_wave = amp*np.array([s_60hz(i) for i in t_array])
 
 s_wave = np.random.default_rng().normal(s_wave, amp/5.0, t_array.size)
-np.savez('s_wave.npz', t_array, s_wave)
+np.savetxt('s_wave.csv', s_wave, delimiter=',')
